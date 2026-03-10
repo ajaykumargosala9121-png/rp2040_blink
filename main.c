a90ae540b2c1586f8eb1b48ac4c2870b8b2d0b2d
ajaykumargosala9121-png <ajaykumargosala9121@gmail.com>
2026-03-10 20:58:00 +0530

    first rp2040 blink project

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+int main()
+{
+    stdio_init_all();
+
+    gpio_init(25);
+    gpio_set_dir(25,GPIO_OUT);
+
+    while(1)
+    {
+        gpio_put(25,1);
+        printf("LED on\n");
+        sleep_ms(1000);
+        gpio_put(25,0);
+        printf("LED off\n");
+        sleep_ms(1000);    
+    }
+}
